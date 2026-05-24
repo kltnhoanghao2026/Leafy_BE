7d7b373bab3aba03e0283ac1ff7667ff82e22b1d
Huyhoang120904 <hoangbeo1124@gmail.com>
2026-05-24 21:52:14 +0700

    fix config-server self config

--- config-server/src/main/java/com/leafy/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/leafy/configserver/ConfigServerApplication.java
@@ -2,14 +2,28 @@ package com.leafy.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    MongoAutoConfiguration.class,
+    MongoDataAutoConfiguration.class,
+    RedisAutoConfiguration.class,
+    RedisRepositoriesAutoConfiguration.class,
+    KafkaAutoConfiguration.class,
+    SecurityAutoConfiguration.class
+})
 @EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
-
 }
